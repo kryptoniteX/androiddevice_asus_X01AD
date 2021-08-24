d37f8e4e7f6e752de64f9632650d4e554daf30b5
Puneet Gautam <punitrajgautam@gmail.com>
2021-08-24 16:32:42 +0530

    X01AD - DT Fix

--- revengeos_X01AD.mk
+++ revengeos_X01AD.mk
@@ -1,27 +1,19 @@
 #
-# Copyright (C) 2021 The RevengeOS Project
+# Copyright (C) 2020 The RevengeOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
+DEVICE_PATH := device/asus/X01AD
+
+# Inherit from source
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from X01AD device
-$(call inherit-product, device/asus/X01AD/device.mk)
+# Set shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common RevengeOS stuff.
+# Inherit common RevengeOS stuff.
 $(call inherit-product, vendor/revengeos/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 720
 TARGET_GAPPS_ARCH := arm64
@@ -31,28 +23,25 @@ IS_PHONE := true
 TARGET_INCLUDE_STOCK_ARCORE := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 
-#RevengeOS Stuff
-REVENGEOS_BUILDTYPE := UNOFFICIAL
-REVENGEOS_MAINTAINER := KryptoniteX
-
-# Set shipping API level
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+# Inherit device stuff
+$(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Device identifiers.
-PRODUCT_NAME := revengeos_X01AD
 PRODUCT_DEVICE := X01AD
-PRODUCT_MANUFACTURER := asus
+PRODUCT_NAME := revengeos_X01AD
 PRODUCT_BRAND := asus
-PRODUCT_MODEL := Asus Zenfone Max M2
-
-PRODUCT_GMS_CLIENTID_BASE := android-asus
-
+PRODUCT_MODEL := ASUS Zenfone Max M2
+PRODUCT_MANUFACTURER := asus
+TARGET_VENDOR := asus
+BOARD_VENDOR := asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRODUCT_MODEL=ASUS_X01AD \
-    PRIVATE_BUILD_DISC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DISC="redfin-user 11 RQ2A.210305.006 7119741 release-keys"
 
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
+
+PRODUCT_GMS_CLIENTID_BASE := android-asus
